023a19a9318018b7f28c7895561541d35872c6e4
Paweł Majczyk <pawel.majczyk@gmail.com>
2018-12-09 21:51:31 +0100

    tests fix

--- src/App.spec.js
+++ src/App.spec.js
@@ -2,26 +2,28 @@ import { shallowMount } from '@vue/test-utils';
 import App from './App.vue';
 
 describe('App', () => {
-  it('renders', () => {
-    const wrapper = shallowMount(App);
+  const wrapper = shallowMount(App);
 
+  it('renders', () => {
     expect(wrapper.html()).toBeTruthy();
   });
   describe('toggles "is-visible" class on click', () => {
-    const wrapper = shallowMount(App);
-    const burger = wrapper.find('[data-test="buger"');
+    const burger = wrapper.find('[data-test="burger"]');
     const nav = wrapper.find('[data-test="nav"]');
 
     it('Does not have is-visible class at start', () => {
       expect(nav.classes('is-visible')).toBe(false);
-      expect(wrapper.emitted('navToggled')).toBeTruthy();
-      expect(wrapper.emitted('navToggled')).toHaveLength(1);
-      expect(wrapper.emitted('navToggled'))[0].toEqual([true]);
+      expect(wrapper.emitted('navToggled')).toBeFalsy();
     });
 
     it('Adds is-visible class on click', () => {
       burger.trigger('click');
-      expect(nav.classes('is-visible')).toBe(true)
-    })
-  })
-})
+
+      expect(nav.classes('is-visible')).toBe(true);
+      expect(wrapper.emitted('navToggled')).toBeTruthy();
+      expect(wrapper.emitted('navToggled')).toHaveLength(1);
+      expect(wrapper.emitted('navToggled')[0]).toEqual([true]);
+    });
+
+  });
+});
